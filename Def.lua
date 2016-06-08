d791a7e50e5dd2929980562d35fa00cbb7495ba1
LightInk <baisaichen@live.com>
2016-06-08 17:42:56 +0800

    fix circlequeue write bug

--- Def.lua
+++ Def.lua
@@ -8,9 +8,6 @@ LightInkDef__:register_def_message(1, 53798, "ControlCmdLineLoader",
 									LightInkTypeDef__.T_String, -- string result //login result
 									nil)
 
--- Control kill Loader
-LightInkDef__:register_def_message(2, 636166, "ControlKillLoader",
-									nil)
 
 -- Control Login Loader
 LightInkDef__:register_def_message(2, 365042, "ControlLoginLoader",
